3fa8e4971e028a405d0667c23e65c3c557959fae
Mangum87 <56420026+Mangum87@users.noreply.github.com>
2020-10-30 23:14:17 -0500

    Add files via upload

--- Code/fitapp/src/main/java/com/fitbitsample/FitbitDataType/OAuthResponse.java
+++ Code/fitapp/src/main/java/com/fitbitsample/FitbitDataType/OAuthResponse.java
@@ -2,10 +2,12 @@ package com.fitbitsample.FitbitDataType;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
-/*
-    Creating a viewing adapter class for parsing gson response for OAUTH 2.0 received from Fitbit API call
- */
 
+import java.util.Calendar;
+import java.util.Date;
+/**
+ * Creating a viewing adapter class for parsing gson response for OAUTH 2.0 received from Fitbit API call
+ */
 public class OAuthResponse {
 
     @SerializedName("access_token")
@@ -26,6 +28,52 @@ public class OAuthResponse {
     @SerializedName("user_id")
     @Expose
     private String userId;
+
+    /**
+     * The time that the OAuth token will expire.
+     */
+    private Date expireTime;
+
+
+    /**
+     * Sets the expiration time of the OAuth token to
+     * be in current time + expires_in seconds.
+     * Default response is 8 hours.
+     */
+    public void setExpireTime()
+    {
+        // Set the expiration time as long from UNIX time.
+        // Set to expire in expiresIn hours.
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, this.expiresIn);
+        this.expireTime = new Date(cal.getTime().getTime());
+    }
+
+    /**
+     * Returns the date of when the OAuth token expires.
+     * @return Date of expiration in seconds
+     */
+    public long getExpireTime()
+    {
+        return this.expireTime.getTime();
+    }
+
+
+    /**
+     * Checks if the token is now or will expire
+     * in the next 60 seconds.
+     * @return True if expired, else false
+     */
+    public boolean isTokenExpired()
+    {
+        Date now = new Date(); // Get current time
+        final int buffer = 60000; // 60 seconds
+
+        if(now.getTime() <= (getExpireTime() - buffer)) // Is current time within 60 seconds of expiration?
+            return false;
+        else
+            return true;
+    }
 
     public String getAccessToken() {
         return accessToken;
