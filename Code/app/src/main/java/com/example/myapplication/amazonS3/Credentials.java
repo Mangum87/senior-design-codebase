8ba6acdfa4715ea628504a2b0691dc1bc8916495
Kcmohan1997 <54957060+Kcmohan1997@users.noreply.github.com>
2020-12-07 23:00:37 -0600

    Update Credentials.java

--- Code/app/src/main/java/com/example/myapplication/amazonS3/Credentials.java
+++ Code/app/src/main/java/com/example/myapplication/amazonS3/Credentials.java
@@ -1,8 +1,8 @@
 package com.example.myapplication.amazonS3;
 
 public class Credentials {
-    private String access_key_id = "AKIA4Q4CIQ6OCZGPBDV3";
-    private String secret_access_key = "uyttT3s2XPZ5HhcoQw+AMxwlFmVpWoxW0eyt0tsA";
+    private String access_key_id = "replace this with yours";
+    private String secret_access_key = "replace this with yours";
 
     public String getAccess_key_id() {
         return access_key_id;
