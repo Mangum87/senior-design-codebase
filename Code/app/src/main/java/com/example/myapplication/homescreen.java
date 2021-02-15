dca2c92f74cbdc87772e943c58434a0d8a3675a4
Mangum87 <56420026+Mangum87@users.noreply.github.com>
2021-02-14 18:59:53 -0600

    Add job sync back in

--- Code/app/src/main/java/com/example/myapplication/homescreen.java
+++ Code/app/src/main/java/com/example/myapplication/homescreen.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
-import android.content.Intent;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
@@ -26,6 +29,8 @@ public class homescreen extends AppCompatActivity {
         bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnNavigationItemSelectedListener(navListener);
         bottomNavigation.setSelectedItemId(R.id.nav_home);
+
+        scheduleJob(); // Schedule auto FitBit sync
     }
 
     //bottom navigation listener
@@ -55,6 +60,32 @@ public class homescreen extends AppCompatActivity {
     };
 
 
+
+    private static final String TAG = "homescreen";
+    public void scheduleJob() {
+        ComponentName componentName = new ComponentName(this, com.example.myapplication.Job.class);
+        JobInfo info = new JobInfo.Builder(123, componentName)
+                .setRequiresCharging(false) //does not require charging
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED) //requires Wi-Fi
+                .setPersisted(true) //start on system restart
+                .setPeriodic(15 * 60 * 1000) //repeat every 15 minutes (Note: Minimum time required is 15 minutes, cannot be lower than 15 minutes.)
+                .build();
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        int resultCode = scheduler.schedule(info);
+        if (resultCode == JobScheduler.RESULT_SUCCESS) {
+            Log.d(TAG, "Job scheduled");
+        } else {
+            Log.d(TAG, "Job scheduling failed");
+        }
+    }
+
+    public void cancelJob() {
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        scheduler.cancel(123);
+        Log.d(TAG, "Job cancelled ");
+    }
+
+
 // commented to go to dashboard without login
 //    @Override
 //    protected void onStart() {
@@ -65,4 +96,4 @@ public class homescreen extends AppCompatActivity {
 //            startActivity(intent);
 //        }
 //    }
-} //-un comment
+}
